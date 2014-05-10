8e64b7282af3f5f588793bf9fb6121323d06df1d
luoguochu <luoguochu@revenco.com>
2014-05-10 13:25:02 +0800

    sendmail

--- sendmail.py
+++ sendmail.py
@@ -1,9 +1,17 @@
-from django.template.loader import *
-from django.template import *
-
+#!/usr/bin/env python
+#coding: utf-8
+import os
+import sys
 import smtplib
 from email.mime.text import MIMEText
+import time
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tinytrue.settings")
+
+from django.template.loader import *
+from django.template import *
 from tinylog.models import *
+
 
 SMTP_HOST = 'smtp.qq.com'
 SMTP_PORT = 465
@@ -13,6 +21,10 @@ SMTP_PASS = '=-8/abc123'
 
 SMTP_FROM = '624038220@qq.com'
 SMTP_NOTIFY = '450171094@qq.com'
+
+LOOP_PAUSE_TIME = 120
+
+MY_SITE = 'www.bigfalse.com'
 
 def send_mail(comment):    
     try:
@@ -26,40 +38,55 @@ def send_mail(comment):
             d['mail_content'] = comment.content
             d['notify_commenter'] = True
             d['ref_content'] = comment.parent.content
-            d['comment_link'] = tinytrue.settings.MY_SITE + '/passage/' + str(comment.passage.id) +  '#comment_content_' + str(comment.id)
+            d['comment_link'] = MY_SITE + '/passage/' + str(comment.passage.id) +  '#comment_content_' + str(comment.id)
             d['comment_link_title'] = d['comment_link']   
             c = Context(d)
             tocmmt_content = t.render(c)
             tocmmt_email = comment.parent.email
         
         toau_content = None
-        toau_email = tinytrue.settings.SMTP_NOTIFY
+        toau_email = SMTP_NOTIFY
         d['mail_content'] = comment.content
         d['notify_commenter'] = False
-        d['comment_link'] = tinytrue.settings.MY_SITE + '/passage/' + str(comment.passage.id) +  '#comment_content_' + str(comment.id)
+        d['comment_link'] = MY_SITE + '/passage/' + str(comment.passage.id) +  '#comment_content_' + str(comment.id)
         d['comment_link_title'] = d['comment_link']
         c = Context(d)
         toau_content = t.render(c)
-        
-        print toau_content
-        
-        smtp = smtplib.SMTP_SSL(host=tinytrue.settings.SMTP_HOST, port=tinytrue.settings.SMTP_PORT)
-        smtp.login(tinytrue.settings.SMTP_USER, tinytrue.settings.SMTP_PASS)
+                
+        smtp = smtplib.SMTP_SSL(host=SMTP_HOST, port=SMTP_PORT)
+        smtp.login(SMTP_USER, SMTP_PASS)
         
         if tocmmt_content != None:
             msg = MIMEText(tocmmt_content, 'html', 'utf-8')        
             msg['Subject'] = u'你的评论有人回复'
-            msg['From'] = tinytrue.settings.SMTP_USER
+            msg['From'] = SMTP_USER
             msg['To'] = tocmmt_email       
-            smtp.sendmail(tinytrue.settings.SMTP_FROM, [tocmmt_email], msg.as_string())
+            smtp.sendmail(SMTP_FROM, [tocmmt_email], msg.as_string())
         
         if toau_content != None:
             msg = MIMEText(toau_content, 'html', 'utf-8')        
             msg['Subject'] = u'你的文章有人回复'
-            msg['From'] = tinytrue.settings.SMTP_USER
+            msg['From'] = SMTP_USER
             msg['To'] = toau_email       
-            smtp.sendmail(tinytrue.settings.SMTP_FROM, [toau_email], msg.as_string())
+            smtp.sendmail(SMTP_FROM, [toau_email], msg.as_string())
         
         smtp.quit()
     except Exception, e:
         print e
+        return False
+        
+    return True
+        
+def main():
+    while True:
+        comments = Comment.objects.filter(is_notify=False)
+        for comment in comments:
+            s = send_mail(comment)
+            print 'send mail, ret = ' + str(s)
+            if s == True:
+                comment.is_notify = True
+                comment.save()
+        print 'sleeping'
+        time.sleep(LOOP_PAUSE_TIME)
+if __name__ == '__main__':
+    main()
